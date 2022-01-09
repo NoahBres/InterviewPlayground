bcda457be42a72f49353cb7a6d0851ee52354146
Noah Bresler <noahbresler2002@gmail.com>
2022-01-09 16:56:31 -0500

    Sieve of eratosthenes that only runs through odd numbers

--- misc/sieve.py
+++ misc/sieve.py
@@ -1,5 +1,9 @@
+# Basic Sieve of Eratosthenes
 def sieve1(limit):
-    a = [True] * limit  # Initialize the primality list
+    if limit < 2:
+        return
+
+    a = [True] * (limit)  # Initialize the primality list
     a[0] = a[1] = False
 
     for (i, isprime) in enumerate(a):
@@ -9,9 +13,30 @@ def sieve1(limit):
                 a[n] = False
 
 
+# Basic Sieve of Eratosthenes
+# Only generate odd numbers
+def sieve2(limit):
+    if limit < 2:
+        return
+
+    yield 2
+
+    half = (limit - 1) // 2
+
+    a = [True] * half  # Initialize the primality list
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            val = i * 2 + 3
+            yield val
+
+            for n in range(val ** 2, limit + 1, val):  # Mark factors non-prime
+                if n % 2 != 0:
+                    a[n // 2 - 1] = False
+
+
 if __name__ == "__main__":
     import timeit
 
-    print(timeit.timeit("sieve1(1000000)", globals=locals()))
-
-    # print(list(sieve1(1000000)))
+    print(timeit.timeit("sieve1(10000000000)", globals=locals()))
+    print(timeit.timeit("sieve2(10000000000)", globals=locals()))
